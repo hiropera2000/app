6d6a5fffe99f16e9ca74070cb284635f80b936cc
Koichi Hirose <hirobo1982@gmail.com>
2016-02-28 21:53:56 +0900

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+10.times do |no|
+  d = rand(10) + 1
+  j = rand(10) + 1
+  s = (rand(22) + 18)*100
+  Emp.create(:empno => no, :ename => "emp #{no}", :depno => d, :jobno => j)
+  Sal.create(:empno => no, :finyear => "2015", :salary => s)
+end
+
+10.times do |no|
+  Dept.create(:depno => no, :dname => "Dep #{no}")
+end
+
+10.times do |no|
+  Job.create(:jobno => no, :jobname => "Job #{no}")
+end
+
+Query.create(:id => "1", :q_num => "1", :q_select => "SELECT * FROM SALS")
+
