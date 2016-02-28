9b67bcaf0f3dae4ce0a57ca648b2385b80eebb15
Koichi Hirose <hirobo1982@gmail.com>
2016-02-28 17:00:44 +0900

    foreign_keyに"を追加

--- app/models/nick.rb
+++ app/models/nick.rb
@@ -1,4 +1,4 @@
 class Nick < ActiveRecord::Base
-   belongs_to :male_line,   class_name: "Sireline", foreign_key: male_id
-   belongs_to :female_line, class_name: "Sireline", foreign_key: female_id
+   belongs_to :male_line,   class_name: "Sireline", foreign_key: "male_id"
+   belongs_to :female_line, class_name: "Sireline", foreign_key: "female_id"
 end
--- app/models/sireline.rb
+++ app/models/sireline.rb
@@ -1,5 +1,5 @@
 class Sireline < ActiveRecord::Base
-  has_many :male_lines,   class_name: "Nick", foreign_key: :male_id
-  has_many :female_lines, class_name: "Nick", foreign_key: :female_id
+  has_many :male_lines,   class_name: "Nick", foreign_key: "male_id"
+  has_many :female_lines, class_name: "Nick", foreign_key: "female_id"
   #has_many :nicks, through: :male_lines
 end
